220aa8a567cb86434c3913a4ac93152079c8cff9
David Otterdahl <david.otterdahl@gmail.com>
2022-10-07 14:23:47 +0200

    Sphinx: Removes numfig extension and use the built in numfig option instead
    The numfig extension works incorrectly and will stop working with later
    versions of python.

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,10 @@ sys.path.insert(0, os.path.abspath('extensions'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['numfig', 'figtable', 'sphinx.ext.mathjax']
+extensions = ['figtable', 'sphinx.ext.mathjax']
+
+# Numbered figures
+numfig = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
