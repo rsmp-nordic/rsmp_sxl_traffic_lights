ef76fe50c21d87e51565690cfd2565b49e381722
Emil Tin <emil@tin.dk>
2026-06-11 12:02:42 +0200

    fix versions/links in changelog

--- source/conf.py
+++ source/conf.py
@@ -87,7 +87,7 @@ extlinks = {
     'issue': ('https://github.com/rsmp-nordic/rsmp_sxl_traffic_lights/issues/%s', '[%s]'),
     'schema_issue': ('https://github.com/rsmp-nordic/rsmp_schema/issues/%s', '[%s]'),
     'pull': ('https://github.com/rsmp-nordic/rsmp_sxl_traffic_lights/pulls/%s', '[%s]'),
-    'compare': ('https://github.com/rsmp-nordic/rsmp_core/compare/%s', '[%s]')
+    'compare': ('https://github.com/rsmp-nordic/rsmp_sxl_traffic_lights/compare/%s', '[%s]')
 }
 
 # -- Options for HTML output ----------------------------------------------
